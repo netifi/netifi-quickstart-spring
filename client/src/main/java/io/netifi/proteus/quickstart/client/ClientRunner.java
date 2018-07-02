15da1f72617f40e5f87d6eff57a6defece106762
Robert Roeser <rroeserr@gmail.com>
2018-07-02 15:15:52 -0700

    ping pong solution

--- client/src/main/java/io/netifi/proteus/quickstart/client/ClientRunner.java
+++ client/src/main/java/io/netifi/proteus/quickstart/client/ClientRunner.java
@@ -7,6 +7,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 /**
  * Calls the Hello Service
@@ -15,23 +18,25 @@ import org.springframework.stereotype.Component;
 public class ClientRunner implements CommandLineRunner {
     private static final Logger logger = LogManager.getLogger(ClientRunner.class);
     
-    
     @ProteusClient(group = "quickstart.services.helloservices")
     private HelloServiceClient client;
     
     @Override
     public void run(String... args) throws Exception {
-        // Create Request to HelloService
-        HelloRequest request = HelloRequest.newBuilder()
-                                   .setName("World")
-                                   .build();
-    
-        logger.info("Sending 'World' to HelloService...");
-    
-        // Call the HelloService
-        logger.info(client.sayHello(request).block());
-        
-        // Exit
-        System.exit(0);
+        Flux.interval(Duration.ofSeconds(1)) // emit tick ever second
+            .onBackpressureBuffer() // buffer ticks on back-pressure
+            .concatMap(
+                l -> {
+                    String name = "World-" + l;
+                    // Create Request to HelloService
+                    HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+                
+                    logger.info("Sending '{}' to HelloService...", name);
+                    return client.sayHello(request).doOnNext(response -> {
+                        logger.info("Got response '{}'", response.getMessage());
+                    });
+                }) // allow one at a lime
+            .doOnError(Throwable::printStackTrace)
+            .blockLast();
     }
 }
