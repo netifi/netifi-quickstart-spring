8b02142e0641890ded06683d7c2019f0df21f051
Ryland Degnan <ryland@netifi.com>
2018-09-06 23:26:36 -0700

    Update dependencies

--- client/src/main/java/io/netifi/proteus/quickstart/client/ClientRunner.java
+++ client/src/main/java/io/netifi/proteus/quickstart/client/ClientRunner.java
@@ -1,8 +1,8 @@
 package io.netifi.proteus.quickstart.client;
 
-import io.netifi.proteus.annotations.ProteusClient;
 import io.netifi.proteus.quickstart.service.protobuf.HelloRequest;
 import io.netifi.proteus.quickstart.service.protobuf.HelloServiceClient;
+import io.rsocket.rpc.annotations.Client;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
@@ -14,9 +14,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class ClientRunner implements CommandLineRunner {
     private static final Logger logger = LogManager.getLogger(ClientRunner.class);
-    
-    
-    @ProteusClient(group = "quickstart.services.helloservices")
+
+    @Client(group = "quickstart.services.helloservices")
     private HelloServiceClient client;
     
     @Override
--- client/src/main/java/io/netifi/proteus/quickstart/client/Main.java
+++ client/src/main/java/io/netifi/proteus/quickstart/client/Main.java
@@ -1,11 +1,9 @@
 package io.netifi.proteus.quickstart.client;
 
-import com.netifi.proteus.springboot.EnableProteus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableProteus(group = "quickstart.clients")
 public class Main {
 
   public static void main(String... args) {
