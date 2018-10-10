d5cc83bb3abf7e2e2bf754ff1891891b51c0325b
Maksym Ostroverkhov <m.ostroverkhov@gmail.com>
2018-10-10 18:20:53 +0300

    ci

--- ci/run.sh
+++ ci/run.sh
@@ -1,5 +1,5 @@
-GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git clone --single-branch -b master ssh://git@github.com:netifi/quickstarts-smoke-test.git runner
-cd runner
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git clone --single-branch -b master ssh://git@github.com:netifi/quickstarts-smoke-test.git runner2
+cd runner2
 ./gradlew app:runMain --args 'netifi/proteus:latest https://github.com/netifi/proteus-spring-quickstart/ci'
 cd ..
-rm -rf runner
+rm -rf runner2
