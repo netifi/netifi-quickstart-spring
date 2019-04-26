745bdfb62be6c57076eaadccfe9ebd8bc5ef9415
Maksym Ostroverkhov <m.ostroverkhov@gmail.com>
2019-04-26 17:27:59 +0300

    gradle: update dependencies to 1.6.1

--- client/src/main/java/io/netifi/proteus/quickstart/client/ClientRunner.java
+++ client/src/main/java/io/netifi/proteus/quickstart/client/ClientRunner.java
@@ -1,42 +1,42 @@
 package io.netifi.proteus.quickstart.client;
 
-import io.netifi.proteus.annotations.ProteusClient;
 import io.netifi.proteus.quickstart.service.protobuf.HelloRequest;
 import io.netifi.proteus.quickstart.service.protobuf.HelloServiceClient;
+import io.netifi.proteus.spring.core.annotation.Group;
+import java.time.Duration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
-import java.time.Duration;
-
-/**
- * Calls the Hello Service
- */
+/** Calls the Hello Service */
 @Component
 public class ClientRunner implements CommandLineRunner {
-    private static final Logger logger = LogManager.getLogger(ClientRunner.class);
-    
-    @ProteusClient(group = "quickstart.services.helloservices")
-    private HelloServiceClient client;
-    
-    @Override
-    public void run(String... args) throws Exception {
-        Flux.interval(Duration.ofSeconds(1)) // emit tick ever second
-            .onBackpressureBuffer() // buffer ticks on back-pressure
-            .concatMap(
-                l -> {
-                    String name = "World-" + l;
-                    // Create Request to HelloService
-                    HelloRequest request = HelloRequest.newBuilder().setName(name).build();
-                
-                    logger.info("Sending '{}' to HelloService...", name);
-                    return client.sayHello(request).doOnNext(response -> {
+  private static final Logger logger = LogManager.getLogger(ClientRunner.class);
+
+  @Group("quickstart.services.helloservices")
+  private HelloServiceClient client;
+
+  @Override
+  public void run(String... args) throws Exception {
+    Flux.interval(Duration.ofSeconds(1)) // emit tick ever second
+        .onBackpressureBuffer() // buffer ticks on back-pressure
+        .concatMap(
+            l -> {
+              String name = "World-" + l;
+              // Create Request to HelloService
+              HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+
+              logger.info("Sending '{}' to HelloService...", name);
+              return client
+                  .sayHello(request)
+                  .doOnNext(
+                      response -> {
                         logger.info("Got response '{}'", response.getMessage());
-                    });
-                }) // allow one at a lime
-            .doOnError(Throwable::printStackTrace)
-            .blockLast();
-    }
+                      });
+            }) // allow one at a lime
+        .doOnError(Throwable::printStackTrace)
+        .blockLast();
+  }
 }
